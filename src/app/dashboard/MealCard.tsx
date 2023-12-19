1356a6d8a01d46459cfe8c45a1b81df24dd4266a
Daniel Maher <danielmaher01@gmail.com>
2023-12-18 21:48:34 -0500

    changed backgroudn to white. styled mealcard components. In the middle of working on button functionality for adding food to each meal

---
+++ src/app/dashboard/MealCard.tsx
@@ -0,0 +1,20 @@
+type cardProps = {
+  mealType: string;
+};
+
+export default function Card({ mealType }: cardProps) {
+  return (
+    <div className="w-11/12 mx-10 rounded-lg border-2">
+      <div className="flow-root pt-2 pl-2">
+        <div className="float-left">{mealType}</div>
+        <button
+          type="submit"
+          value="+"
+          className="w-10 rounded-lg float-right mr-6 mb-2 border-2"
+        >
+          +
+        </button>
+      </div>
+    </div>
+  );
+}
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,27 @@
+import { useState } from "react";
 import Image from "next/image";
+import MealCard from "./MealCard";
 
 export default function Home() {
-  return <></>;
+  // const [foods, setFood] = useState([]);
+
+  // const addFood = (food: any) => {
+  //   if (!food.text || /^\s*$/.test(food.text)) {
+  //     return;
+  //   }
+
+  //   const newFood = [food, ...foods];
+  // };
+
+  return (
+    <>
+      <div className="h-96">Header</div>
+      <MealCard mealType="Breakfast" />
+      <MealCard mealType="Mid-Morning Snack" />
+      <MealCard mealType="Lunch" />
+      <MealCard mealType="Afternoon Snack" />
+      <MealCard mealType="Dinner" />
+      <MealCard mealType="Dessert" />
+    </>
+  );
 }

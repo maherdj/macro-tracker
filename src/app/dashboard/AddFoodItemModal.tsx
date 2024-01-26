6f2d0cb517593b730cd6faa6512ad42fa03d9cdc
Daniel Maher <danielmaher01@gmail.com>
2024-01-26 16:43:42 -0500

    cleaned up commented out code and added function to toggle new entry line after hiting submit

--- src/app/dashboard/AddFoodItemModal.tsx
+++ src/app/dashboard/AddFoodItemModal.tsx
@@ -145,35 +145,17 @@ export default function AddFoodItemModal({
     setProtein(0);
     setFat(0);
     setCarbs(0);
-    console.log(isOpen);
+    toggle();
   };
-
-  // const handleChange = (e) => {
-
-  // }
 
   return (
     <>
       {isOpen && (
-        // <div>
-        //   <div>{props.children}</div>
-        // </div>
         <div className="w-full col-span-12 h-auto bg-white">
-          {/* <div className="flow-root">
-            {/* //       <button
-            onClick={() => {
-              close();
-            }}
-            className="mr-2 p-2 mt-2 rounded-lg border-2 float-right justify-end"
-          >
-            X
-          </button> */}
-
           <form
             className="grid grid-cols-8 grid-rows-1 p-5 gap-y-2"
             onSubmit={handleAdd}
           >
-            {/* <div className="w-auto col-span-1"></div> */}
             <input
               className="col-span-1 border-2"
               type="text"
@@ -211,17 +193,13 @@ export default function AddFoodItemModal({
 
             <button
               className="w-auto col-span-1 border-2 rounded-lg mx-2"
-              // type="sutton"
               value="submit"
             >
               Submit
             </button>
-            {/* <div>{error}</div> */}
           </form>
         </div>
       )}
     </>
-    //   <div onClick={() => setIsOpen(false)} />
-    //
   );
 }

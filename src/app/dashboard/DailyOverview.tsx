860ac34aa12a6ee3fce15437f5b1b6890ca12922
Daniel Maher <danielmaher01@gmail.com>
2024-01-30 20:27:10 -0500

    added useEffect to calculate total macros and calories for the day. Next step, putting data into an object to be ingested by pie chart

--- src/app/dashboard/DailyOverview.tsx
+++ src/app/dashboard/DailyOverview.tsx
@@ -1,15 +1,109 @@
 "use client";
 
 import { PieChart } from "@mui/x-charts";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useMacroContext } from "./context";
 
 export default function DailyOverview() {
+  const {
+    breakfastMacros,
+    setBreakfastMacros,
+    breakfastFoods,
+    setBreakfastFoods,
+    morningSnackFoods,
+    setMorningSnackFoods,
+    lunchFoods,
+    setLunchFoods,
+    afternoonSnackFoods,
+    setAfternoonSnackFoods,
+    dinnerFoods,
+    setDinnerFoods,
+    dessertFoods,
+    setDessertFoods,
+    foodName,
+    setFoodName,
+    protein,
+    setProtein,
+    fat,
+    setFat,
+    carbs,
+    setCarbs,
+    calories,
+    setCalories,
+  } = useMacroContext();
   const data = [
     { label: "Group A", value: 400 },
     { label: "Group B", value: 300 },
     { label: "Group C", value: 300 },
     { label: "Group D", value: 200 },
   ];
+
+  const [dailyProtein, setDailyProtein] = useState<number>(0);
+
+  const [dailyFat, setDailyFat] = useState<number>(0);
+  const [dailyCarbs, setDailyCarbs] = useState<number>(0);
+  const [dailyCalories, setDailyCalories] = useState<number>(0);
+
+  useEffect(() => {
+    let protein: number = 0;
+    let carbs: number = 0;
+    let fats: number = 0;
+    let cals: number = 0;
+
+    breakfastFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    morningSnackFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    lunchFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    afternoonSnackFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    dinnerFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    dessertFoods.map((food) => {
+      protein = protein + food.proteinContent;
+      carbs = carbs + food.carbContent;
+      fats = fats + food.fatContent;
+      cals = cals + food.calories;
+    });
+
+    setDailyProtein(protein);
+    setDailyCarbs(carbs);
+    setDailyFat(fats);
+    setDailyCalories(cals);
+  }, [
+    breakfastFoods,
+    morningSnackFoods,
+    lunchFoods,
+    afternoonSnackFoods,
+    dinnerFoods,
+    dessertFoods,
+  ]);
   return (
     <div className="flex items-center justify-center p-7 m-7 border-4  rounded-xl ">
       <PieChart

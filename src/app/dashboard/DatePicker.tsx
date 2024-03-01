193df9460a92fe802ea2e3b058c442d716962516
Daniel Maher <danielmaher01@gmail.com>
2024-02-29 22:31:55 -0500

    redid datepicker code. Got rid of MUI component and added in shadcn component. router.push command was appending 'dashbord/[date] every time submit was clicked. Added slash in front of dashboard to make that stop'

--- src/app/dashboard/DatePicker.tsx
+++ src/app/dashboard/DatePicker.tsx
@@ -1,23 +1,83 @@
-import { DesktopDatePicker, LocalizationProvider } from "@mui/x-date-pickers";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import dayjs, { Dayjs } from "dayjs";
-import React, { useEffect, useState } from "react";
+import { useMacroContext } from "./context";
+import { Calendar as CalendarIcon } from "lucide-react";
+import { format } from "date-fns";
+import { cn } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { Calendar } from "@/components/ui/calendar";
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover";
+
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export default function DatePicker() {
-  const [date, setDate] = React.useState<Dayjs | null>(dayjs());
+  const { date, setDate } = useMacroContext();
+
   const router = useRouter();
 
-  const handleChange = () => {
+  useEffect(() => {
+    setDate(new Date());
+  }, []);
+
+  const handleChange = (e: React.FormEvent) => {
+    e.preventDefault();
     let dateString: string;
-    router.push(`dashboard/${date}`);
+    let dateDay: number;
+    let dateMonth: number;
+    let dateYear: number;
+
+    dateDay = date.getDay();
+    dateMonth = date.getMonth();
+    dateYear = date.getFullYear();
+
+    dateString =
+      dateMonth.toString() + dateDay.toString() + dateYear.toString();
+
+    console.log(
+      "Month:" +
+        dateMonth +
+        "   " +
+        "Day: " +
+        dateDay +
+        "   " +
+        "Year: " +
+        dateYear
+    );
+    setDate(date);
+
+    router.push(`/dashboard/${dateString}`);
+
+    console.log("date: " + dateString);
   };
 
   return (
-    <div>
-      <LocalizationProvider dateAdapter={AdapterDayjs}>
-        <DesktopDatePicker value={date} onChange={handleChange} />
-      </LocalizationProvider>
-    </div>
+    <form onSubmit={handleChange}>
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            variant={"outline"}
+            className={cn(
+              "w-[280px] justify-start text-left font-normal",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date ? format(date, "PPP") : <span>Pick a date</span>}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0">
+          <Calendar
+            mode="single"
+            selected={date}
+            onSelect={setDate}
+            initialFocus
+          />
+        </PopoverContent>
+      </Popover>
+      <button>submit</button>
+    </form>
   );
 }
